90e6e5ee9b4ef95b54e70fd23b1170429d0f8577
abhishek.lashley <abhishek.lashley@gmail.com>
2022-09-01 13:15:36 +0530

    Initial commit

---
+++ src/StudentImplUsingHashSet.java
@@ -0,0 +1,68 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class StudentImplUsingHashSet
+{
+
+    public static void main(String[] args)
+    {
+
+        StudentImplUsingHashSet studentImplUsingHashSet=new StudentImplUsingHashSet();
+
+        Set<Student> students=studentImplUsingHashSet.createSet();
+        studentImplUsingHashSet.display(students);
+
+        Boolean result = studentImplUsingHashSet.removeByRollNumber(students,12);
+
+        if(result)
+        {
+            System.out.println("Student Deleted");
+        }
+        else
+        {
+            System.out.println("Student not found");
+        }
+
+        studentImplUsingHashSet.display(students);
+        
+    }
+
+    public Set<Student> createSet()
+    {
+        Set<Student> students = new HashSet<>();
+
+        students.add(new Student("Vaibhav",12,100));
+        students.add(new Student("Ramesh",20,90));
+        students.add(new Student("Suresh",25,80));
+
+        return students;
+    }
+
+    public void display(Set<Student> student)
+    {
+        Iterator<Student> iterator = student.iterator();
+        while(iterator.hasNext())
+        {
+            System.out.println(iterator.next());
+        }
+    }
+
+    public boolean removeByRollNumber(Set<Student> students,int rollNo)
+    {
+        boolean response=false;
+        Iterator<Student> iterator= students.iterator();
+        while(iterator.hasNext())
+        {
+            if(iterator.next().getRollNo()==rollNo)
+            {
+                iterator.remove();
+                response=true;
+                break;
+            }
+        }
+
+        return response;
+
+    }
+}
